fda03e4e7448b35ff31cd06ca83610eddbd002f9
Sudhanshu Kumar <sudhiskpll@gmail.com>
2022-04-20 12:28:44 +0530

    completed mern shopping

--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI : 'mongodb+srv://sudhanshu:sudhi237@mernshopping.zu6e8.gcp.mongodb.net/mernShopping?retryWrites=true&w=majority'
-};
--- server.js
+++ server.js
@@ -1,26 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const path = require('path');
-
-const items = require('./routes/api/items');
+const config = require('config');
 
 const app = express();
 
-// Bodyparser Middleware
-app.use(bodyParser.json());
+// Bodyparser Middleware // express includes a body parser now
+app.use(express.json());
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+const db = config.get('mongoURI');
 
 // Connect to Mongo
 mongoose
-    .connect(db)
+    .connect(db,{
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    })  // Adding new mongo url parser
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
 // Use Routes
-app.use('/api/items',items);
+app.use('/api/items',require('./routes/api/items'));
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'))
 
 // Serve  static assets if in production
 if(process.env.NODE_ENV === 'production') {
